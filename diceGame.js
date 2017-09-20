49c47bf4961ce305c28b876b00daa951274cf15e
mbinelli22 <mbinelli1996@gmail.com>
2017-09-20 14:09:17 -0500

    added new instructions

--- diceGame.js
+++ diceGame.js
@@ -1,15 +1,22 @@
 "use strict"
 
-// roll all the dice at the same time and add up total. If you roll a 1, 5, 10, 15, 
-// or 20 on the 20 sided die (divisible by 5. or 1) you go back to zero and have to take a drink. first one 
-// to 100 wins.
+// player rolls all the dice at the same time except the 4 sided and the 20 sided dice. those two will be rolled seperatly right after.
+//first, the player will add up the total of all the dice. 
+// whatever number is rolled on the 4 sided die = number of seconds you drink your kool aid for.
+// if a 5, 10, 15 or 20 are rolled on the 20 sided die then->
+// 5 = player who rolled, drinks kool aid for the amount of seconds rolled on the 4 die and you loose all your points
+// 10 = player gets to pick another player to drink kool aid for x amount of seconds
+// 15 = everyone else drinks kool aid for x amount of seconds
+// 20 = player gets to pick another player to drink kool aid for x amount of seconds and that player also looses all their points
+// players only get one turn per round and play until someone gets 100 points.
 
-// let rollDie1 = "4 sided die"
-// let rollDie2 = "6 sided die"
-// let rollDie3 = "8 sided die"
-// let rollDie4 = "10 sided die"
-// let rollDie5 = "12 sided die"
-// let rollDie6 = "20 sided die"
+
+let rollDie1 = "4 sided die"
+let rollDie2 = "6 sided die"
+let rollDie3 = "8 sided die"
+let rollDie4 = "10 sided die"
+let rollDie5 = "12 sided die"
+let rollDie6 = "20 sided die"
 
 let player1 = prompt("Enter player 1 name here: \nFive player maximum.");
 let player2 = prompt("Enter player 2 name here: \nFive player maximum.");
@@ -40,12 +47,12 @@ let rollDie6 = Math.floor(Math.random()*20 + 1);
 
 
 
-console.log ("4 sided die; You rolled a: "+rollDie1+"");
-console.log ("6 sided die; You rolled a: "+rollDie2+"");
-console.log ("8 sided die; You rolled a: "+rollDie3+"");
-console.log ("10 sided die; You rolled a: "+rollDie4+"");
-console.log ("12 sided die; You rolled a: "+rollDie5+"");
-console.log ("20 sided die; You rolled a: "+rollDie6+"");
+console.log ("4 sided die -> You rolled a: "+rollDie1+"");
+console.log ("6 sided die -> You rolled a: "+rollDie2+"");
+console.log ("8 sided die -> You rolled a: "+rollDie3+"");
+console.log ("10 sided die -> You rolled a: "+rollDie4+"");
+console.log ("12 sided die -> You rolled a: "+rollDie5+"");
+console.log ("20 sided die -> You rolled a: "+rollDie6+"");
 
 
 let score = rollDie1+rollDie2+rollDie3+rollDie4+rollDie5+rollDie6
@@ -70,19 +77,15 @@ let diceRoll6=(rollDie6);
 switch (diceRoll6) {
     case 5:
         alert("You rolled a 5! \nDRINK! \nAnd go back to zero points.");
-        score=(0);
         break;
     case 10:
         alert("You rolled a 10! \nDRINK! \nAnd go back to zero points.");
-        score=(0);
         break;
     case 15:
     	alert("You rolled a 15! \nDRINK! \nAnd go back to zero points.");
-    	score=(0);
     	break;
     case 20:
     	alert("You rolled a 20! \nDRINK! \nAnd go back to zero points.");
-    	score=(0);
     	break;
     default:
         
@@ -98,5 +101,5 @@ let body = document.getElementsByTagName("body")[0];
 body.appendChild(button);
 
 button.addEventListener("click", function () {
-	alert('did something');
+	alert('die rolled bih');
 });
