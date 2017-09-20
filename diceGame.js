7ffaaa2622127816157431b3b37bfc17ecf4da9a
mbinelli22 <mbinelli1996@gmail.com>
2017-09-20 10:38:27 -0500

    added a switch case function

--- diceGame.js
+++ diceGame.js
@@ -11,11 +11,18 @@
 // let rollDie5 = "12 sided die"
 // let rollDie6 = "20 sided die"
 
-// let player1 = prompt("Enter player 1 name here: ");
-// let player2 = prompt("Enter player 2 name here: ");
-// let player3 = prompt("Enter player 3 name here: ");
-// let player4 = prompt("Enter player 4 name here: ");
-// let player5 = prompt("Enter player 5 name here: ");
+// let player1 = prompt("Enter player 1 name here: \nFive player maximum.");
+// let player2 = prompt("Enter player 2 name here: \nFive player maximum.");
+// let player3 = prompt("Enter player 3 name here: \nFive player maximum.");
+// let player4 = prompt("Enter player 4 name here: \nFive player maximum.");
+// let player5 = prompt("Enter player 5 name here: \nFive player maximum.");
+
+// console.log(player1);
+// console.log(player2);
+// console.log(player3);
+// console.log(player4);
+// console.log(player5);
+
 
 let rollDie1 = Math.floor(Math.random()*4 + 1);
 let rollDie2 = Math.floor(Math.random()*6 + 1);
@@ -24,14 +31,49 @@ let rollDie4 = Math.floor(Math.random()*10 + 1);
 let rollDie5 = Math.floor(Math.random()*12 + 1);
 let rollDie6 = Math.floor(Math.random()*20 + 1);
 
+
+
+console.log ("4 sided die; You rolled a: "+rollDie1+"");
+console.log ("6 sided die; You rolled a: "+rollDie2+"");
+console.log ("8 sided die; You rolled a: "+rollDie3+"");
+console.log ("10 sided die; You rolled a: "+rollDie4+"");
+console.log ("12 sided die; You rolled a: "+rollDie5+"");
+console.log ("20 sided die; You rolled a: "+rollDie6+"");
+
 let score = rollDie1+rollDie2+rollDie3+rollDie4+rollDie5+rollDie6
-
-console.log ("You rolled a: "+rollDie1+"");
-console.log ("You rolled a: "+rollDie2+"");
-console.log ("You rolled a: "+rollDie3+"");
-console.log ("You rolled a: "+rollDie4+"");
-console.log ("You rolled a: "+rollDie5+"");
-console.log ("You rolled a: "+rollDie6+"");
-
 console.log ("Your score is:" + score);
 
+// let button = document.createElement("button");
+// button.innerHTML = "Roll die";
+
+// function diceRoll(){
+// 	let randomNumber = rollDie1;
+// 	return randomNumber;
+// }
+// function printNumber(number){
+
+// }
+
+
+let diceRoll6=(rollDie6);
+
+switch (diceRoll6) {
+    case 1:
+        alert("You rolled a 1! \nDRINK! \nAnd go back to zero points.");
+        break;
+    case 5:
+        alert("You rolled a 5! \nDRINK! \nAnd go back to zero points.");
+        break;
+    case 10:
+        alert("You rolled a 10! \nDRINK! \nAnd go back to zero points.");
+        break;
+    case 15:
+    	alert("You rolled a 15! \nDRINK! \nAnd go back to zero points.");
+    	break;
+    case 20:
+    	alert("You rolled a 20! \nDRINK! \nAnd go back to zero points.");
+    	break;
+    default:
+        
+        break;
+}
