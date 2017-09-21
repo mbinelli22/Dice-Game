3c380f3a0b62810663fb756307a6de2bad8946e8
mbinelli22 <mbinelli1996@gmail.com>
2017-09-20 21:57:21 -0500

    minor changes, trying to get function player1Score to work

--- diceGame.js
+++ diceGame.js
@@ -18,30 +18,42 @@
 // let rollDie5 = "12 sided die"
 // let rollDie6 = "20 sided die"
 
-// let player1 = prompt("Enter player 1 name here: \nFive player maximum.");
-// let player2 = prompt("Enter player 2 name here: \nFive player maximum.");
-// let player3 = prompt("Enter player 3 name here: \nFive player maximum.");
-// let player4 = prompt("Enter player 4 name here: \nFive player maximum.");
-// let player5 = prompt("Enter player 5 name here: \nFive player maximum.");
+let player1 = prompt("Enter player 1 name here: \nFive player maximum.");
+let player2 = prompt("Enter player 2 name here: \nFive player maximum.");
+let player3 = prompt("Enter player 3 name here: \nFive player maximum.");
+let player4 = prompt("Enter player 4 name here: \nFive player maximum.");
+let player5 = prompt("Enter player 5 name here: \nFive player maximum.");
 
 // create a funtion that puts names into an array
 
-// 	let playerNamesArr = [player1, player2, player3, player4, player5];
-// 	console.log("Player 1: " + playerNamesArr[0]);
-// 	console.log("Player 2: " + playerNamesArr[1]);
-// 	console.log("Player 3: " + playerNamesArr[2]);
-// 	console.log("Player 4: " + playerNamesArr[3]);
-// 	console.log("Player 5: " + playerNamesArr[4]);
+	let playerNamesArr = [player1, player2, player3, player4, player5];
+	console.log("Player 1: " + playerNamesArr[0]);
+	console.log("Player 2: " + playerNamesArr[1]);
+	console.log("Player 3: " + playerNamesArr[2]);
+	console.log("Player 4: " + playerNamesArr[3]);
+	console.log("Player 5: " + playerNamesArr[4]);
 
 // make a function that rolls all the dice
+		let playerOneRoll = prompt("To roll the dice type YES");
+		let score = rollDie1+rollDie2+rollDie3+rollDie4+rollDie5+rollDie6
+function player1Score(playerOneRoll,score) {
+	if (playerOneRoll = "yes") {
+		console.log ("4 sided die -> You rolled a: "+rollDie1+"");
+		console.log ("6 sided die -> You rolled a: "+rollDie2+"");
+		console.log ("8 sided die -> You rolled a: "+rollDie3+"");
+		console.log ("10 sided die -> You rolled a: "+rollDie4+"");
+		console.log ("12 sided die -> You rolled a: "+rollDie5+"");
+		console.log ("20 sided die -> You rolled a: "+rollDie6+"");
 
-function diceRollForButton(argument) {
-	// body...
+			if (rollDie6 === 5){
+				score = (0);
+				}
+			if (rollDie6 === 20){
+				score = (0);
+				}
+		console.log (player1"'s score is:" + score());
+		}
 }
-
-
-
-
 // make a function that keeps track of score for each person
 
 // console.log(player1);
@@ -60,23 +72,7 @@ let rollDie6 = Math.floor(Math.random()*20 + 1);
 
 
 
-console.log ("4 sided die -> You rolled a: "+rollDie1+"");
-console.log ("6 sided die -> You rolled a: "+rollDie2+"");
-console.log ("8 sided die -> You rolled a: "+rollDie3+"");
-console.log ("10 sided die -> You rolled a: "+rollDie4+"");
-console.log ("12 sided die -> You rolled a: "+rollDie5+"");
-console.log ("20 sided die -> You rolled a: "+rollDie6+"");
 
-
-let score = rollDie1+rollDie2+rollDie3+rollDie4+rollDie5+rollDie6
-
-if (rollDie6 === 5){
-	score = (0);
-}
-if (rollDie6 === 20){
-	score = (0);
-}
-console.log ("Your score is:" + score);
 
 
 let diceRoll6=(rollDie6);
@@ -101,12 +97,12 @@ switch (diceRoll6) {
 
 // button shit
 
-let button = document.createElement("button");
-button.innerHTML = "Roll Die";
+// let button = document.createElement("button");
+// button.innerHTML = "Roll Die";
 
-let body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
+// let body = document.getElementsByTagName("body")[0];
+// body.appendChild(button);
 
-button.addEventListener("click", function () {
-	alert('die rolled');
-});
+// button.addEventListener("click", function () {
+// 	alert('die rolled');
+// });
