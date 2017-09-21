ec48e0b49a854517695ae9e87c55df8b5818e0ed
mbinelli22 <mbinelli1996@gmail.com>
2017-09-21 13:45:21 -0500

    made the dice roll function to work

--- diceGame.js
+++ diceGame.js
@@ -1,16 +1,5 @@
 "use strict"
 
-// player rolls all the dice at the same time except the 4 sided and the 20 sided dice. those two will be rolled seperatly right after.
-//first, the player will add up the total of all the dice. 
-// whatever number is rolled on the 4 sided die = number of seconds you drink your kool aid for.
-// if a 5, 10, 15 or 20 are rolled on the 20 sided die then->
-// 5 = player who rolled, drinks kool aid for the amount of seconds rolled on the 4 die and you loose all your points
-// 10 = player gets to pick another player to drink kool aid for x amount of seconds
-// 15 = everyone else drinks kool aid for x amount of seconds
-// 20 = player gets to pick another player to drink kool aid for x amount of seconds and that player also looses all their points
-// players only get one turn per round and play until someone gets 100 points.
-
-
 // let rollDie1 = "4 sided die"
 // let rollDie2 = "6 sided die"
 // let rollDie3 = "8 sided die"
@@ -18,62 +7,58 @@
 // let rollDie5 = "12 sided die"
 // let rollDie6 = "20 sided die"
 
-let player1 = prompt("Enter player 1 name here: \nFive player maximum.");
-let player2 = prompt("Enter player 2 name here: \nFive player maximum.");
-let player3 = prompt("Enter player 3 name here: \nFive player maximum.");
-let player4 = prompt("Enter player 4 name here: \nFive player maximum.");
-let player5 = prompt("Enter player 5 name here: \nFive player maximum.");
+// let player1 = prompt("Enter player 1 name here: \nFive player maximum.");
+// let player2 = prompt("Enter player 2 name here: \nFive player maximum.");
+// let player3 = prompt("Enter player 3 name here: \nFive player maximum.");
+// let player4 = prompt("Enter player 4 name here: \nFive player maximum.");
+// let player5 = prompt("Enter player 5 name here: \nFive player maximum.");
 
-// create a funtion that puts names into an array
+// // create a funtion that puts names into an array
 
-	let playerNamesArr = [player1, player2, player3, player4, player5];
-	console.log("Player 1: " + playerNamesArr[0]);
-	console.log("Player 2: " + playerNamesArr[1]);
-	console.log("Player 3: " + playerNamesArr[2]);
-	console.log("Player 4: " + playerNamesArr[3]);
-	console.log("Player 5: " + playerNamesArr[4]);
+// 	let playerNamesArr = [player1, player2, player3, player4, player5];
+// 	console.log("Player 1: " + playerNamesArr[0]);
+// 	console.log("Player 2: " + playerNamesArr[1]);
+// 	console.log("Player 3: " + playerNamesArr[2]);
+// 	console.log("Player 4: " + playerNamesArr[3]);
+// 	console.log("Player 5: " + playerNamesArr[4]);
+// make a function that capitalizes first letter in player names.
+// let userInput = prompt("Enter a phrase yo:");
 
-// make a function that rolls all the dice
-		let playerOneRoll = prompt("To roll the dice type YES");
-		let score = rollDie1+rollDie2+rollDie3+rollDie4+rollDie5+rollDie6
-function player1Score(playerOneRoll,score) {
-	if (playerOneRoll = "yes") {
-		console.log ("4 sided die -> You rolled a: "+rollDie1+"");
-		console.log ("6 sided die -> You rolled a: "+rollDie2+"");
-		console.log ("8 sided die -> You rolled a: "+rollDie3+"");
-		console.log ("10 sided die -> You rolled a: "+rollDie4+"");
-		console.log ("12 sided die -> You rolled a: "+rollDie5+"");
-		console.log ("20 sided die -> You rolled a: "+rollDie6+"");
+// function capitalizeWords(phraseToCapitalize) {
+// 	let playerNamesArr = phraseToCapitalize.split(" ");
+// 	for (let i = 0; i < playerNamesArr.length; i++) {
+// 		playerNamesArr[i] = playerNamesArr[i].charAt(0).toUpperCase() + playerNamesArr[i].slice(1);
+// 	}
+// 	return playerNamesArr.join(" ");
+// }
 
-			if (rollDie6 === 5){
-				score = (0);
-				}
-			if (rollDie6 === 20){
-				score = (0);
-				}
-		console.log (player1"'s score is:" + score());
-		}
+// var answer = capitalizeWords;
+// console.log(answer);
+
+// make a function that rolls all the dice and gets a new number everytime
+		
+
+function diceRoll() {
+	let rollDie1 = Math.floor(Math.random()*4 + 1);
+	let rollDie2 = Math.floor(Math.random()*6 + 1);
+	let rollDie3 = Math.floor(Math.random()*8 + 1);
+	let rollDie4 = Math.floor(Math.random()*10 + 1);
+	let rollDie5 = Math.floor(Math.random()*12 + 1);
+	let rollDie6 = Math.floor(Math.random()*20 + 1);
+	console.log ("4 sided die -> You rolled a: "+rollDie1+"");
+	console.log ("6 sided die -> You rolled a: "+rollDie2+"");
+	console.log ("8 sided die -> You rolled a: "+rollDie3+"");
+	console.log ("10 sided die -> You rolled a: "+rollDie4+"");
+	console.log ("12 sided die -> You rolled a: "+rollDie5+"");
+	console.log ("20 sided die -> You rolled a: "+rollDie6+"");
+	let score = (rollDie1+rollDie2+rollDie3+rollDie4+rollDie5+rollDie6);
+	return score;
 }
-// make a function that keeps track of score for each person
+let newDiceRoll = diceRoll();
 
-// console.log(player1);
-// console.log(player2);
-// console.log(player3);
-// console.log(player4);
-// console.log(player5);
+console.log("Your new score is: " + newDiceRoll);
 
-
-let rollDie1 = Math.floor(Math.random()*4 + 1);
-let rollDie2 = Math.floor(Math.random()*6 + 1);
-let rollDie3 = Math.floor(Math.random()*8 + 1);
-let rollDie4 = Math.floor(Math.random()*10 + 1);
-let rollDie5 = Math.floor(Math.random()*12 + 1);
-let rollDie6 = Math.floor(Math.random()*20 + 1);
-
-
-
-
-
+// let playerOneRoll = prompt(playerNamesArr[0]+"'s turn! \nTo roll the dice type YES");
 
 let diceRoll6=(rollDie6);
 
@@ -95,14 +80,41 @@ switch (diceRoll6) {
         break;
 }
 
-// button shit
+// let playerTwoRoll = prompt(playerNamesArr[1]+"'s turn! \nTo roll the dice type YES");
+// function player2Score(playerTwoRoll,score) {
+// 	if (playerTwoRoll === "yes") {
+// 		console.log ("4 sided die -> You rolled a: "+rollDie1+"");
+// 		console.log ("6 sided die -> You rolled a: "+rollDie2+"");
+// 		console.log ("8 sided die -> You rolled a: "+rollDie3+"");
+// 		console.log ("10 sided die -> You rolled a: "+rollDie4+"");
+// 		console.log ("12 sided die -> You rolled a: "+rollDie5+"");
+// 		console.log ("20 sided die -> You rolled a: "+rollDie6+"");
+// 		}	
+// 	}
+// 	if (rollDie6 === 5){
+// 		score = (0);
+// 		}
+// 	if (rollDie6 === 20){
+// 		score = (0);
+// 		}
+// 		console.log(playerNamesArr[1]+"'s score is:" + score);
 
-// let button = document.createElement("button");
-// button.innerHTML = "Roll Die";
 
-// let body = document.getElementsByTagName("body")[0];
-// body.appendChild(button);
-
-// button.addEventListener("click", function () {
-// 	alert('die rolled');
-// });
+// let playerThreeRoll = prompt(playerNamesArr[2]+"'s turn! \nTo roll the dice type YES");
+// function player3Score(playerThreeRoll,score) {
+// 	if (playerThreeRoll === "yes") {
+// 		console.log ("4 sided die -> You rolled a: "+rollDie1+"");
+// 		console.log ("6 sided die -> You rolled a: "+rollDie2+"");
+// 		console.log ("8 sided die -> You rolled a: "+rollDie3+"");
+// 		console.log ("10 sided die -> You rolled a: "+rollDie4+"");
+// 		console.log ("12 sided die -> You rolled a: "+rollDie5+"");
+// 		console.log ("20 sided die -> You rolled a: "+rollDie6+"");
+// 		}	
+// 	}
+// 	if (rollDie6 === 5){
+// 		score = (0);
+// 		}
+// 	if (rollDie6 === 20){
+// 		score = (0);
+// 		}
+// 		console.log(playerNamesArr[2]+"'s score is:" + score);		
